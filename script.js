8a2dde0bc9cd0b9e826e369df4b6f7ab783182ae
Tim Goryuk <tgoryuk@kenroyhome.com>
2022-11-29 10:02:26 -0500

    updated colors2

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
 function loadCalendar() {
-    //alert("Hello! I am an alert box!!");
 
-    var array = ["slide 1", "slide 2", "slide 3", "slide 4", "slide 5"];
-    var yearPadding = 2;
+    var year = 2022
+    var yearPadding = 3;
     
     for (i = 0 + yearPadding; i < 365 + yearPadding; i++) {
     var li = document.createElement("li");
-    var text = document.createTextNode(dayToDate(2023, i));
+    var text = document.createTextNode(dayToDate(year, i));
     li.appendChild(text);
-
+    li.style.setProperty('background',monthColor(year, i),'');
     
+    const today = new Date();
+    const calDate = new Date(year, 0, i);
 
-    li.style.setProperty('background',monthColor(2023, i),'');
+    if (today.toDateString() === calDate.toDateString()){
+        li.style.setProperty('background','red','');
+    }
 
     document.getElementById("day-grid").appendChild(li);
     }
@@ -19,9 +22,17 @@ function loadCalendar() {
 }
 
 function monthColor(year, day) {
-    colors1 = ["#fafa6e","#cdef72","#a4e27a","#7dd382","#58c389","#35b28e","#0ea18f","#008f8c","#007d85","#146b79","#23596a","#2a4858"]
+    
+    var color1 = "#eeeeee"
+    var color2 = "#ffffff"
+/*
+    var color1 = "#1F7A8C"
+    var color2 = "#728f95"
 
-    colors = ["#1F7A8C","#728f95","#1F7A8C","#728f95","#1F7A8C","#728f95","#1F7A8C","#728f95","#1F7A8C","#728f95","#1F7A8C","#728f95"]
+    */
+    var colorgradeient = ["#fafa6e","#cdef72","#a4e27a","#7dd382","#58c389","#35b28e","#0ea18f","#008f8c","#007d85","#146b79","#23596a","#2a4858"]
+
+    var colors = [color1,color2,color1,color2,color1,color2,color1,color2,color1,color2,color1,color2]
 
     const date = new Date(year, 0, day);
     let monthnum = date.getMonth()
@@ -33,8 +44,9 @@ function monthColor(year, day) {
 function dayToDate(year, day) {
     const date = new Date(year, 0, day);
     let dayofmonth = date.getDate();
-    let month = date.getMonth();
-    let text = month + '/' + dayofmonth;  
+    /*let month = date.getMonth() + 1;
+    let text = month + '/' + dayofmonth */;  
+    let text = dayofmonth;
     return text;
   }
 
